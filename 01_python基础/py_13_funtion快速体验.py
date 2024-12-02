b191e16afd9028283e8c2a02eeed7a010ad7b913
sunweiye.3 <sunweiye.3@bytedance.com>
2024-12-03 01:03:36 +0800

    format

--- 01_python基础/py_13_funtion快速体验.py
+++ 01_python基础/py_13_funtion快速体验.py
@@ -2,6 +2,8 @@
 # 调用其他模块(py文件)下的函数或者变量应该先将文件引入
 import py_13_funtion
 
-py_13_funtion.chengfa_99()  # 调用此模块中的方法
+if __name__ == '__main__':
 
-print("%d" % py_13_funtion.sun)  # 调用此模块中的全局变量
+    py_13_funtion.chengfa_99()  # 调用此模块中的方法
+
+    print("%d" % py_13_funtion.sun)  # 调用此模块中的全局变量
